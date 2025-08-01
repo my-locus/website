bd0aba95f01f60a5cba22a43234ce74aa83931db
Larry Le <larryle704@gmail.com>
2025-08-01 03:29:02 -0700

    Basic Launch Site
    Basic launch site with includes prefers-color-scheme: dark which allows custom CSS depending on whether you default to light mode or dark mode

---
+++ website/src/app/page.js
@@ -0,0 +1,58 @@
+"use client";
+
+import "./globals.css";
+import Image from "next/image";
+import thumbtackBlack from "../../assets/locus-thumbtack-black.png";
+import thumbtackWhite from "../../assets/locus-thumbtack-white.png";
+
+import { useEffect, useState } from "react";
+
+export default function Home() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    // Check the user's system preference for dark mode (tailwind also uses this under the hood)
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+    setIsDarkMode(mediaQuery.matches);
+
+    const handleChange = (event) => {
+      setIsDarkMode(event.matches);
+    };
+    mediaQuery.addEventListener("change", handleChange);
+
+    // Clean up the event listener when the component unmounts to prevent memory leaks
+    return () => {
+      mediaQuery.removeEventListener("change", handleChange);
+    };
+  }, []);
+
+  return (
+    <div className="w-full min-h-screen flex justify-center gap-3 flex-col items-center outline-1">
+      <div className="flex gap-5 flex-col items-center">
+        <p className="comic-relief text-5xl text-center w-48 ">Locus</p>
+        {isDarkMode && (
+          <Image
+            src={thumbtackWhite}
+            alt="Locus Thumbtack White"
+            width={100}
+            height={100}
+            className="w-32 h-32"
+          />
+        )}
+        {isDarkMode == false && (
+          <Image
+            src={thumbtackBlack}
+            alt="Locus Thumbtack Black"
+            width={100}
+            height={100}
+            className="w-32 h-32"
+          />
+        )}
+      </div>
+
+      <p className="comic-relief text-2xl text-center">
+        Curating experiences for couples and social groups. Coming soon...
+      </p>
+    </div>
+  );
+}
---
+++ website/tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "locus-primary": "#7F5FFF",
+        "locus-secondary": "#7F5FFFBF",
+        "locus-silver": "#F2F2F7",
+        "locus-grey": "#C7C7CC",
+      },
+      fontFamily: {
+        "comic-relief": ["Comic Relief", "sans-serif"],
+      },
+      spacing: {
+        13: "3.25rem",
+      },
+      height: {
+        13: "3.25rem",
+        11.5: "2.875rem",
+      },
+    },
+  },
+  plugins: [],
+};
