12eedbe248e303e7abe3e89f537f2e11503a7b58
aagrawal623 <aadi.agrawal@gmail.com>
2025-10-12 16:09:58 -0700

    Added navbar

--- website/src/app/page.js
+++ website/src/app/page.js
@@ -27,32 +27,44 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="w-full min-h-screen flex justify-center gap-3 flex-col items-center outline-1">
-      <div className="flex gap-5 flex-col items-center">
-        <p className="comic-relief text-5xl text-center w-48 ">Locus</p>
-        {isDarkMode && (
-          <Image
-            src={thumbtackWhite}
-            alt="Locus Thumbtack White"
-            width={100}
-            height={100}
-            className="w-32 h-32"
-          />
-        )}
-        {isDarkMode == false && (
-          <Image
-            src={thumbtackBlack}
-            alt="Locus Thumbtack Black"
-            width={100}
-            height={100}
-            className="w-32 h-32"
-          />
-        )}
-      </div>
-
-      <p className="comic-relief text-2xl text-center">
-        Curating experiences for couples. Coming soon...
-      </p>
+    <div>
+      <header className="bg-violet-400 dark:bg-violet-800">
+        <nav className="container flex items-center justify-between px-6 py-5">
+          <div className="flex items-center gap-x-6 md:gap-x-12">
+            <a href="#">
+              <span className="sr-only">Locus</span>
+              <Image
+                src={thumbtackBlack}
+                alt="Locus Logo"
+                className="h-8 w-auto dark:hidden"
+              />
+              <Image
+                src={thumbtackWhite}
+                alt="Locus Logo"
+                className="h-8 w-auto hidden dark:block"
+              />
+            </a>
+            <a href="#" className="text-base font-semibold text-gray-900 dark:text-white">
+              Features
+            </a>
+            <a href="#" className="text-base font-semibold text-gray-900 dark:text-white">
+              About
+            </a>
+            <a href="/support" className="text-base font-semibold text-gray-900 dark:text-white">
+              Contact
+            </a>
+          </div>
+          
+          <div className="ml-auto">
+            <a href="#" className="flex items-center gap-1 rounded-lg bg-violet-700 px-5 py-2 text-sm font-semibold text-white hover:text-black">
+              <svg className="h-5 w-5" viewBox="0 0 24 24" fill="currentColor">
+                <path d="M18.71 19.5c-.83 1.24-1.71 2.45-3.05 2.47-1.34.03-1.77-.79-3.29-.79-1.53 0-2 .77-3.27.82-1.31.05-2.3-1.32-3.14-2.53C4.25 17 2.94 12.45 4.7 9.39c.87-1.52 2.43-2.48 4.12-2.51 1.28-.02 2.5.87 3.29.87.78 0 2.26-1.07 3.81-.91.65.03 2.47.26 3.64 1.98-.09.06-2.17 1.28-2.15 3.81.03 3.02 2.65 4.03 2.68 4.04-.03.07-.42 1.44-1.38 2.83M13 3.5c.73-.83 1.94-1.46 2.94-1.5.13 1.17-.34 2.35-1.04 3.19-.69.85-1.83 1.51-2.95 1.42-.15-1.15.41-2.35 1.05-3.11z"/>
+              </svg>
+              Download
+            </a>
+          </div>
+        </nav>
+      </header>
     </div>
   );
 }
