07b84ea2043022264bb434830bfd46c334293600
Larry Le <larryle704@gmail.com>
2025-10-12 01:59:46 -0700

    Remove redundant logo asset

--- website/src/app/components/ui/Logo.js
+++ website/src/app/components/ui/Logo.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Image from "next/image";
-import LocusLogo from "../../assets/locus-thumbtack-black.png";
+import LocusLogo from "../../../../assets/locus-thumbtack-black.png";
 
 export default function Logo() {
   return (
     <div className="flex gap-4 items-center">
-      <Image src={LocusLogo} alt="Locus Logo" width={48} height={48} />
+      <Image src={LocusLogo} alt="Locus Logo" width={56} height={56} />
       <p className="comic-relief text-3xl">Locus</p>
     </div>
   );
