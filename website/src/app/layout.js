09fc6d66f0c77ba3adffc5bf699304aaf92ea77b
Larry Le <larryle704@gmail.com>
2025-10-21 12:16:01 -0700

    opengraph and apple icon
    Opengraph and apple icon for metadata crawlers such as Apple's crawler when you paste the website in imessage

--- website/src/app/layout.js
+++ website/src/app/layout.js
@@ -15,6 +15,26 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "Locus",
   description: "A great match deserves more than an awkward coffee date",
+
+  // Open Graph (OG) tags (which web crawlers like Apple's will read when someone shares your)
+  // website
+  openGraph: {
+    // title: 'Your Amazing Page Title', // Will default to metadata.title if not set
+    // description: 'A compelling description...', // Will default to metadata.description
+    // siteName: 'Your Site Name',
+    url: 'https://www.trylocus.app', // The canonical URL
+    
+    images: [
+      {
+        url: 'https://www.trylocus.app/images/your-chosen-preview-image.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'A description of your preview image',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({ children }) {
