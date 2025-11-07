d4063c449fb26eacf3d2303ca0b9140f386f775b
Larry Le <larryle704@gmail.com>
2025-11-07 12:12:24 -0800

    add next.js speed insights

--- website/src/app/layout.js
+++ website/src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -47,8 +48,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Analytics />
         {children}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
